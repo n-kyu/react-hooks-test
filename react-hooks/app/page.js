23c3abe79cceb334f4208fa3edc19e5b88fd4dbf
Nathan Costa <nkyucode@gmail.com>
2023-09-28 17:40:08 -0300

    feat: creating a link to "useeffectpage"

--- react-hooks/app/page.js
+++ react-hooks/app/page.js
@@ -3,13 +3,19 @@ import Link from "next/link";
 export default function Home() {
     return (
         <>
-            <div className="principal-container">
+            <div className="principal-container flex flex-col">
                 <h1 className="font-bold text-5xl mb-12">React Hooks</h1>
                 <Link
-                    className="hover:text-black transform transition"
+                    className="hover:text-black transform transition mb-2"
                     href="/usestatepage"
                 >
                     useState
+                </Link>
+                <Link
+                    className="hover:text-black transform transition mb-2"
+                    href="/useeffectpage"
+                >
+                    useEffect
                 </Link>
             </div>
         </>
